0dabd763be80b1789eccdcb9681224750eff9520
Amrita <Amrita@DESKTOP-E12F0AK>
2025-07-01 15:41:37 +0530

    Add project files.

---
+++ pro1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13014_AMRITA
+{
+    internal class pro1
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("Hello World!!");
+
+            System.Console.Read();
+        }
+    }
+}
