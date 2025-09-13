8d336b6f391947abee15bb3be8b7ce20dd67a2b1
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-13 14:23:18 +0530

    new

--- TUTORIAL3/pra4.cs
+++ TUTORIAL3/pra4.cs
@@ -1,47 +1,49 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿/*4 : Use above program classes and create objects for 5 students and demonstrate the use student class.*/
 
-    namespace _24SOECE213014_AMRITA.TUTORIAL3
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE213014_AMRITA.TUTORIAL3
+{
+    class Student
     {
-        class Student
+        private int enrolmentNo;
+        private string name;
+        private int age;
+
+        public Student(int e, string n, int a)
         {
-            private int enrolmentNo;
-            private string name;
-            private int age;
-
-            public Student(int e, string n, int a)
-            {
-                enrolmentNo = e;
-                name = n;
-                age = a;
-            }
-
-            public void Display()
-            {
-                Console.WriteLine($"EnrolmentNo: {enrolmentNo}, Name: {name}, Age: {age}");
-            }
+            enrolmentNo = e;
+            name = n;
+            age = a;
         }
 
-        class TestStudent
+        public void Display()
         {
-            public static void Main(string[] args)
-            {
-                Student s1 = new Student(101, "Amrita", 20);
-                Student s2 = new Student(102, "Arpita", 15);
-                Student s3 = new Student(103, "Amit", 22);
-                Student s4 = new Student(104, "Komal", 20);
-                Student s5 = new Student(105, "Krishna", 23);
-
-                s1.Display();
-                s2.Display();
-                s3.Display();
-                s4.Display();
-                s5.Display();
-
-                Console.ReadKey();
-            }
+            Console.WriteLine($"EnrolmentNo: {enrolmentNo}, Name: {name}, Age: {age}");
         }
     }
+
+    class TestStudent
+    {
+        public static void Main(string[] args)
+        {
+            Student s1 = new Student(101, "Amrita", 20);
+            Student s2 = new Student(102, "Arpita", 15);
+            Student s3 = new Student(103, "Amit", 22);
+            Student s4 = new Student(104, "Komal", 20);
+            Student s5 = new Student(105, "Krishna", 23);
+
+            s1.Display();
+            s2.Display();
+            s3.Display();
+            s4.Display();
+            s5.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
