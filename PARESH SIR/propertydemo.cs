9dc27e97624ebf0819c349f2eca9901be6c42484
Amrita <Amrita@DESKTOP-E12F0AK>
2025-09-13 11:21:28 +0530

    NEW

---
+++ PARESH SIR/propertydemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE213014_AMRITA.PARESH_SIR
+{
+    class StudentC
+    {
+        private int id;
+        public int getId()
+        {    return id;    }
+
+        public void setId(int id)
+        {    this.id = id;  }
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+    class propertydemo
+    {
+    }
+}
